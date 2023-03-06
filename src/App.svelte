c3c596032ba1b79be597730ef75c7de2b216f27d
principle105 <principle105@gmail.com>
2023-03-05 21:17:43 -0500

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,7 @@
     import { onMount } from "svelte";
     import gsap from "gsap";
     import ScrollTrigger from "gsap/ScrollTrigger";
-
-    import WavingHand from "./assets/wavingHand.svelte";
+    import Landing from "./components/Landing.svelte";
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -25,31 +24,7 @@
         class="w-10 h-16 border-[3px] border-zinc-400 rounded-3xl fixed z-20 bottom-3.5 left-1/2 before:w-3 before:h-3 before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:bg-zinc-100 before:rounded-full before:animate-wheel"
         id="mouse"
     />
-    <div class="m-auto">
-        <div class="flex items-end gap-6">
-            <h1 class="text-white text-8xl font-semibold">Hello!</h1>
-            <div class="h-24">
-                <WavingHand />
-            </div>
-        </div>
-        <div class="mt-6">
-            <div class="text-white text-2xl">
-                I'm a self-taught developer from
-                <span class="text-red font-semibold">Canada</span>
-                who likes building cool things.
-            </div>
-        </div>
-        <div class="mt-10">
-            <a
-                href="https://github.com/principle105"
-                target="_blank"
-                rel="noreferrer"
-                class="text-bg rounded-xl text-[1.075rem] px-9 py-3.5 bg-purple font-medium hover:brightness-90 transition"
-            >
-                Github
-            </a>
-        </div>
-    </div>
+    <Landing />
 </main>
 
 <style global lang="postcss">
---
+++ src/components/Landing.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import WavingHand from "../assets/WavingHand.svelte";
+</script>
+
+<div class="m-auto">
+    <div class="flex items-end gap-6">
+        <h1 class="text-white text-8xl font-semibold">Hello!</h1>
+        <div class="h-24">
+            <WavingHand />
+        </div>
+    </div>
+    <div class="mt-6">
+        <div class="text-zinc-200 text-xl">
+            I'm a self-taught developer from
+            <span class="text-red font-semibold">Canada</span>
+            who likes building cool things.
+        </div>
+    </div>
+    <div class="mt-10">
+        <a
+            href="https://github.com/principle105"
+            target="_blank"
+            rel="noreferrer"
+            class="text-bg rounded-xl text-[1.075rem] px-9 py-3.5 bg-purple font-medium hover:brightness-90 transition"
+        >
+            Github
+        </a>
+    </div>
+</div>
