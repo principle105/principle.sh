46ed488a5cf1e173051f0cee42a462fdc6e5054a
principle105 <principle105@gmail.com>
2023-03-05 21:58:16 -0500

    working?

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
-    plugins: [svelte()],
+    plugins: [svelte(), tsconfigPaths()],
 });
