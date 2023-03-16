041366ea28b38a1af360fd1dbefe91d64bf21058
principle105 <principle105@gmail.com>
2023-03-15 23:56:54 -0400

    better

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount, onDestroy } from "svelte";
-    import Router from "svelte-spa-router";
+    import type { ComponentType } from "svelte/internal";
     import gsap from "gsap";
     import ScrollTrigger from "gsap/ScrollTrigger";
     import Landing from "$components/Landing.svelte";
@@ -9,93 +9,52 @@
     import FaTimes from "svelte-icons/fa/FaTimes.svelte";
     import FaFolder from "svelte-icons/fa/FaFolder.svelte";
     import FaFile from "svelte-icons/fa/FaFile.svelte";
-    import type { ComponentType } from "svelte/internal";
 
-    interface Folder {
-        folderName: string;
-        route: string;
-        files: Route[];
+    interface File {
+        name: string;
+        icon: ComponentType;
     }
 
-    interface Route {
-        fileName: string;
+    interface Route extends File {
         route: string;
-        icon: ComponentType;
         component: ComponentType;
     }
 
+    interface Folder extends Route {
+        files: File[];
+    }
+
     const isFolder = (route: Folder | Route): route is Folder => {
-        return "folderName" in route;
+        return "files" in route;
     };
 
     let aspectRatio: string;
     let scale: number;
 
-    let directory: (Folder | Route)[] = [
+    let directory: (Route | Folder)[] = [
         {
-            folderName: "projects",
-            route: "/projects",
+            name: "projects",
+            route: "#projects",
+            icon: FaFolder,
+            component: Landing,
             files: [
                 {
-                    fileName: "about.md",
-                    route: "/about",
-                    component: Landing,
+                    name: "wordpractice.py",
                     icon: FaFile,
                 },
                 {
-                    fileName: "contact.md",
-                    route: "/contact",
-                    component: Landing,
-                    icon: FaFile,
-                },
-                {
-                    fileName: "contact.md",
-                    route: "/contact",
-                    component: Landing,
+                    name: "thomas-coin.py",
                     icon: FaFile,
                 },
             ],
         },
         {
-            fileName: "about.md",
-            route: "/",
+            name: "about.md",
+            route: "#about",
+            icon: FaFolder,
             component: Landing,
-            icon: FaFile,
-        },
-        {
-            fileName: "contact.md",
-            route: "/contact",
-            component: Landing,
-            icon: FaFile,
-        },
-        {
-            fileName: "contact.md",
-            route: "/contact",
-            component: Landing,
-            icon: FaFile,
         },
     ];
-
-    const getRoutes = () => {
-        let routes = {};
-
-        for (let i = 0; i < directory.length; i++) {
-            let item = directory[i];
-
-            if (isFolder(item)) {
-                for (let j = 0; j < item.files.length; j++) {
-                    let file = item.files[j];
-                    routes[item.route + file.route] = file.component;
-                }
-            } else {
-                routes[item.route] = item.component;
-            }
-        }
-
-        return routes;
-    };
-
-    let routes = getRoutes();
 
     let currentPage: Route = directory[1] as Route;
 
@@ -150,7 +109,7 @@
         <div
             class="w-full py-2 flex items-center border-b-2 border-border_main"
         >
-            <div class="flex gap-1.5 ml-2">
+            <div class="flex ml-2 gap-1.5">
                 <div class="h-3 w-3 rounded-full bg-[#ed6a5e]" />
                 <div class="h-3 w-3 rounded-full bg-[#f5be4f]" />
                 <div class="h-3 w-3 rounded-full bg-[#61c554]" />
@@ -168,7 +127,7 @@
                     <h4 class="uppercase text-xs text-zinc-300">EXPLORER</h4>
                 </div>
                 <div
-                    class="pl-3 pr-16 gap-1.5 w-full h-7 border-b-2 border-border_main flex text-white items-center"
+                    class="pl-3 pr-16 w-full h-7 border-b-2 border-border_main flex text-white items-center gap-1.5"
                 >
                     <div class="h-4">
                         <FaAngleRight />
@@ -188,20 +147,20 @@
                 <div class="text-white text-sm mt-3">
                     {#each directory as route}
                         {#if isFolder(route)}
-                            <div class="flex gap-1.5 items-center pl-6 py-1">
+                            <div class="flex items-center pl-6 py-1 gap-1.5">
                                 <div class="h-4">
                                     <FaAngleDown />
                                 </div>
                                 <div class="h-4 text-zinc-400">
                                     <FaFolder />
                                 </div>
-                                <h5>{route.folderName}</h5>
+                                <h5>{route.name}</h5>
                             </div>
                             <div class="pl-6 py-1">
                                 {#each route.files as file}
                                     <h6
-                                        class="pl-6 py-1 flex gap-1.5 items-center {currentPage.route ===
-                                        file.route
+                                        class="pl-6 py-1 flex items-center gap-1.5 {currentPage.name ===
+                                        file.name
                                             ? 'bg-highlight'
                                             : ''}"
                                     >
@@ -210,13 +169,15 @@
                                                 this={file.icon}
                                             />
                                         </div>
-                                        {file.fileName}
+                                        <span>
+                                            {file.name}
+                                        </span>
                                     </h6>
                                 {/each}
                             </div>
                         {:else}
                             <h5
-                                class="pl-6 py-1 flex gap-1.5 items-center {currentPage.route ===
+                                class="pl-6 py-1 flex items-center gap-1.5 {currentPage.route ===
                                 route.route
                                     ? 'bg-highlight'
                                     : ''}"
@@ -224,7 +185,9 @@
                                 <div class="h-4">
                                     <svelte:component this={route.icon} />
                                 </div>
-                                {route.fileName}
+                                <span>
+                                    {route.name}
+                                </span>
                             </h5>
                         {/if}
                     {/each}
@@ -233,13 +196,13 @@
             <div class="grow">
                 <div class="h-11 w-full flex">
                     <div
-                        class="h-full bg-main_light w-max flex items-center px-3 py-1 relative gap-3 border-border_main border-r-2"
+                        class="h-full bg-main_light w-max flex items-center gap-3 px-3 py-1 relative border-border_main border-r-2"
                     >
                         <div
                             class="absolute top-0 left-0 h-0.5 w-full bg-cyan"
                         />
                         <span class="text-vs text-cyan">
-                            {currentPage.fileName}
+                            {currentPage.name}
                         </span>
                         <div class="h-4 text-white">
                             <FaTimes />
@@ -248,24 +211,24 @@
                     <div class="border-b-2 border-border_main grow" />
                 </div>
                 <div
-                    class="text-comment text-xs flex items-center gap-2 pl-3 py-1 bg-main_light"
+                    class="text-comment text-xs flex items-center pl-3 py-1 bg-main_light gap-2"
                 >
                     <span>folder</span>
                     <div class="h-4">
                         <FaAngleRight />
                     </div>
-                    <span>{currentPage.fileName}</span>
+                    <span>{currentPage.name}</span>
                 </div>
                 <div
                     class="bg-main_light flex"
                     style="aspect-ratio: {aspectRatio}"
                     id="content"
                 >
-                    <Router {routes} />
+                    <Landing />
                 </div>
             </div>
         </div>
-        <footer class="border-t-2 border-border_main w-full">
+        <footer class="border-t-2 border-border_main w-full flex items-center">
             <span class="text-purple opacity-50 text-sm">footer</span>
         </footer>
     </div>
