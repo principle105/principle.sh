95578ab46716b8baf28a2ed895d4f42e7f113083
principle105 <principle105@gmail.com>
2023-03-19 05:55:23 -0400

    update

--- src/App.svelte
+++ src/App.svelte
@@ -15,23 +15,23 @@
         icon: ComponentType;
     }
 
-    interface Route extends File {
+    interface Folder extends File {
         route: string;
         component: ComponentType;
-    }
-
-    interface Folder extends Route {
         files: File[];
     }
-
-    const isFolder = (route: Folder | Route): route is Folder => {
-        return "files" in route;
-    };
 
     let aspectRatio: string;
     let scale: number;
 
-    let directory: (Route | Folder)[] = [
+    let directory: Folder[] = [
+        {
+            name: "about",
+            route: "#about",
+            icon: FaFolder,
+            component: Landing,
+            files: [],
+        },
         {
             name: "projects",
             route: "#projects",
@@ -49,14 +49,22 @@
             ],
         },
         {
-            name: "about.md",
-            route: "#about",
+            name: "contact",
+            route: "#contact",
             icon: FaFolder,
             component: Landing,
+            files: [],
         },
     ];
 
-    let currentPage: Route = directory[1] as Route;
+    let currentDirIndex: number = 0;
+    let currentFileIndex: number = 0;
+
+    $: currentDir = directory[currentDirIndex];
+    $: currentFile =
+        currentDir.files.length == 0
+            ? null
+            : currentDir.files[currentFileIndex];
 
     const setAspectRatio = () => {
         const width = window.innerWidth;
@@ -79,20 +87,20 @@
             opacity: 0,
             scrollTrigger: {
                 trigger: "#mouse",
-                start: "top bottom",
-                end: "bottom bottom",
+                start: "top bottom-=100",
+                end: "bottom bottom-=100",
                 scrub: true,
             },
         });
 
-        // gsap.from("#editor", {
-        //     scale,
-        //     scrollTrigger: {
-        //         trigger: "#window",
-        //         pin: "#window",
-        //         scrub: true,
-        //     },
-        // });
+        gsap.from("#editor", {
+            scale,
+            scrollTrigger: {
+                trigger: "#window",
+                pin: "#window",
+                scrub: true,
+            },
+        });
     });
 
     onDestroy(() => {
@@ -102,14 +110,14 @@
 
 <main class="w-screen h-screen" id="window">
     <div
-        class="w-10 h-16 border-[3px] border-zinc-400 rounded-3xl fixed z-20 bottom-3.5 left-1/2 before:w-3 before:h-3 before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:bg-zinc-100 before:rounded-full before:animate-wheel"
+        class="w-10 h-16 border-[3px] border-zinc-400 rounded-3xl fixed z-20 bottom-3 left-1/2 before:w-3 before:h-3 before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:bg-zinc-100 before:rounded-full before:animate-wheel"
         id="mouse"
     />
     <div class="flex-col flex origin-test" id="editor">
         <div
             class="w-full py-2 flex items-center border-b-2 border-border_main"
         >
-            <div class="flex ml-2 gap-1.5">
+            <div class="flex ml-2 gap-2">
                 <div class="h-3 w-3 rounded-full bg-[#ed6a5e]" />
                 <div class="h-3 w-3 rounded-full bg-[#f5be4f]" />
                 <div class="h-3 w-3 rounded-full bg-[#61c554]" />
@@ -118,14 +126,16 @@
                 about.md - Principle@dev
             </h2>
         </div>
-        <div class="grow overflow-hidden flex">
+        <div class="grow flex">
             <div
                 class="w-[3.15rem] border-r-2 border-border_main hidden xl:block"
             />
             <div class="min-w-max border-r-2 border-border_main">
-                <div class="w-full flex pl-6">
-                    <h4 class="uppercase text-xs text-zinc-300">EXPLORER</h4>
-                </div>
+                <h4
+                    class="w-full py-3 flex pl-6 uppercase text-xs text-zinc-300"
+                >
+                    EXPLORER
+                </h4>
                 <div
                     class="pl-3 pr-16 w-full h-7 border-b-2 border-border_main flex text-white items-center gap-1.5"
                 >
@@ -137,76 +147,63 @@
                     </h4>
                 </div>
                 <h4
-                    class="uppercase text-white text-xs font-semibold mt-3 ml-3 flex items-center gap-1.5"
+                    class="uppercase text-white text-xs font-semibold mt-1.5 mb-1 ml-3 flex items-center gap-1.5"
                 >
                     <div class="h-4">
                         <FaAngleDown />
                     </div>
                     <span>Principle@dev</span>
                 </h4>
-                <div class="text-white text-sm mt-3">
-                    {#each directory as route}
-                        {#if isFolder(route)}
-                            <div
-                                class="flex items-center pl-5 pr-3 py-1 gap-1.5"
-                            >
-                                <div class="h-4">
+                <div class="text-white text-xs">
+                    {#each directory as dir}
+                        <div class="flex items-center pl-5 pr-3 py-1 gap-1.5">
+                            <div class="h-4">
+                                {#if dir.files.length == 0}
+                                    <FaAngleRight />
+                                {:else}
                                     <FaAngleDown />
-                                </div>
-                                <div class="h-4 text-zinc-400">
-                                    <FaFolder />
-                                </div>
-                                <h5>{route.name}</h5>
+                                {/if}
                             </div>
-                            <div class="pl-5 py-1">
-                                {#each route.files as file}
-                                    <h6
-                                        class="pl-5 pr-3 py-1 flex items-center gap-1.5 {currentPage.name ===
-                                        file.name
-                                            ? 'bg-highlight'
-                                            : ''}"
-                                    >
-                                        <div class="h-4">
-                                            <svelte:component
-                                                this={file.icon}
-                                            />
-                                        </div>
-                                        <span>
-                                            {file.name}
-                                        </span>
-                                    </h6>
-                                {/each}
+                            <div class="h-4 text-zinc-400">
+                                <FaFolder />
                             </div>
-                        {:else}
-                            <h5
-                                class="pl-6 py-1 flex items-center gap-1.5 {currentPage.route ===
-                                route.route
+                            <h5>{dir.name}</h5>
+                        </div>
+                        {#each dir.files as file}
+                            <h6
+                                class="pl-10 pr-3 py-1 flex items-center gap-1.5 {currentDir.name ===
+                                file.name
                                     ? 'bg-highlight'
                                     : ''}"
                             >
                                 <div class="h-4">
-                                    <svelte:component this={route.icon} />
+                                    <svelte:component this={file.icon} />
                                 </div>
                                 <span>
-                                    {route.name}
+                                    {file.name}
                                 </span>
-                            </h5>
-                        {/if}
+                            </h6>
+                        {/each}
                     {/each}
                 </div>
             </div>
             <div class="grow">
-                <div class="h-11 w-full flex">
+                <div class="h-10 w-full flex">
                     <div
                         class="h-full bg-main_light w-max flex items-center gap-3 px-3 py-1 relative border-border_main border-r-2"
                     >
                         <div
                             class="absolute top-0 left-0 h-0.5 w-full bg-cyan"
                         />
-                        <span class="text-vs text-cyan">
-                            {currentPage.name}
-                        </span>
-                        <div class="h-4 text-white">
+                        <div class="flex gap-2 items-center">
+                            <div class="h-4 text-zinc-400">
+                                <svelte:component this={currentDir.icon} />
+                            </div>
+                            <span class="text-vs text-cyan text-xs">
+                                {currentDir.name}
+                            </span>
+                        </div>
+                        <div class="h-3.5 text-white">
                             <FaTimes />
                         </div>
                     </div>
@@ -215,11 +212,13 @@
                 <div
                     class="text-comment text-xs flex items-center pl-3 py-1 bg-main_light gap-2"
                 >
-                    <span>folder</span>
-                    <div class="h-4">
-                        <FaAngleRight />
-                    </div>
-                    <span>{currentPage.name}</span>
+                    {#if currentFile !== null}
+                        <span>{currentFile.name}</span>
+                        <div class="h-4">
+                            <FaAngleRight />
+                        </div>
+                    {/if}
+                    <span>{currentDir.name}</span>
                 </div>
                 <div
                     class="bg-main_light flex"
@@ -230,7 +229,9 @@
                 </div>
             </div>
         </div>
-        <footer class="border-t-2 border-border_main w-full flex items-center">
+        <footer
+            class="border-t-2 border-border_main w-full flex items-center max-h-min"
+        >
             <span class="text-purple opacity-50 text-sm">footer</span>
         </footer>
     </div>
